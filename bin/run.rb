77ac2efe92c0f4754b59ee61df5dae9953b8bc11
David <davidspector08@gmail.com>
2020-05-05 13:37:08 -0400

    [Yupdate]

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,6 @@
 require_relative '../config/environment'
+require "tty-prompt"
+
 
 def greet_user (name)
     puts "Please enter your name."
@@ -13,7 +15,7 @@ greet_user(name)
 def create_playlist (playlist_name)
     puts "Name your playlist."
     playlist_name = gets.strip
-    Playlist.create(name: playlist_name)
+    playlist = Playlist.create(name: playlist_name)
     puts "Your playlist #{playlist_name} is created."
 end
 
@@ -21,6 +23,10 @@ playlist_name = playlist_name
 create_playlist(playlist_name)
 
 def add_songs_to_playlist
-    puts Song.all.name
-    puts "Which songs would you like to add to your playlist?"
+    prompt = TTY::Prompt.new
+    choices = Song.all
+    selected_songs = prompt.multi_select("Which songs would you like to add to your playlist?", choices)
+    PlaylistSong.create(song: selected_songs.song_id, playlist: playlist.playlist_id)
 end
+
+add_songs_to_playlist
