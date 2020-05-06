dd4be4e10e8ad2ab8032549001462751b0aca0e8
blobbyblobfish <aywang6@gmail.com>
2020-05-06 09:36:28 -0400

    Finished MVP.

--- bin/run.rb
+++ bin/run.rb
@@ -5,18 +5,18 @@ require 'pry'
 $prompt = TTY::Prompt.new
 
 
-def greet_user (name)
+def greet_user
     puts "Please enter your name."
     name = gets.strip
     puts "Hello, #{name}"
     $user = User.create(name: name)
 end
 
-def create_playlist (playlist_name)
+def create_playlist 
     puts "Name your playlist."
     playlist_name = gets.strip
-    $playlist = Playlist.create(name: playlist_name, user_id: $user.id)
     puts "Your playlist #{playlist_name} is created."
+    $playlist = Playlist.create(name: playlist_name, user_id: $user.id)
 end
 
 def actions
@@ -43,31 +43,24 @@ def view_playlist
 end
 
 def add_song_to_playlist
-    all_songs = Song.all
-    selected_song = $prompt.select("Which song would you like to add to your playlist?", all_songs.map {|song| song.name})
-    selected_song_object = all_songs.find {|song| song.name == selected_song}
+    selected_song = $prompt.select("Which song would you like to add to your playlist?", Song.all.map {|song| song.name})
+    selected_song_object = Song.all.find {|song| song.name == selected_song}
     new_playlistsong = PlaylistSong.create(song_id: selected_song_object.id, playlist_id: $playlist.id)
     
     view_playlist
 end
 
 def delete_song
-    playlist_songs = $playlist.songs
-    selected_song = $prompt.select("Which song would you like to delete?", playlist_songs.map {|song| song.name})
+    selected_song = $prompt.select("Which song would you like to delete?", $playlist.songs.map {|song| song.name})
     selected_song_object = Song.all.find {|song| song.name == selected_song}
-    selected_playlist_song = PlaylistSong.all.find {|playlistsong| playlistsong.song_id == selected_song_object.id}
-    PlaylistSong.delete(selected_playlist_song.id)
+    selected_playlistsong = $playlist.playlist_songs.find {|playlistsong| playlistsong.song_id == selected_song_object.id}
+    PlaylistSong.destroy(selected_playlistsong.id)
 
     view_playlist
 end
 
-name = name
-greet_user(name)
-
-playlist_name = playlist_name
-create_playlist(playlist_name)
-
+greet_user
+create_playlist
 add_song_to_playlist
-actions
-actions
+
 
